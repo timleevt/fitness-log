db0ff6de42a35d58217932e7073cba5c2a280cca
Tim Lee <timleevt@gmail.com>
2022-12-21 20:10:01 +0900

    added photos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const [show, setShow] = useState(false);
   const [text, setText] = useState("");
   const handleMouseEnter = (position: number) => {
     if (position === 1) {
@@ -14,38 +13,28 @@ export default function Home() {
     } else if (position === 4) {
       setText("favorite technique/position: open guard / berimbolo");
     }
-    setShow(true);
-  };
-
-  // Currently bugged. If you mouse over the text it will call this repeatedly
-  const handleMouseLeave = () => {
-    setShow(false);
   };
 
   return (
     <div className={styles.container}>
-      <div className={styles.imgContainer}>
+      <div className={styles.imgContainer} onMouseLeave={() => setText("")}>
         <div
           className={styles.bgImgCard}
           onMouseEnter={() => handleMouseEnter(1)}
-          onMouseOut={() => handleMouseLeave()}
         />
         <div
           className={styles.bgImgCard}
           onMouseEnter={() => handleMouseEnter(2)}
-          onMouseOut={() => handleMouseLeave()}
         />
         <div
           className={styles.bgImgCard}
           onMouseEnter={() => handleMouseEnter(3)}
-          onMouseOut={() => handleMouseLeave()}
         />
         <div
           className={styles.bgImgCard}
           onMouseEnter={() => handleMouseEnter(4)}
-          onMouseOut={() => handleMouseLeave()}
         />
-        {show && <div className={styles.hoverTxt}>{text}</div> }
+        <div className={styles.hoverTxt}>{text}</div>
       </div>
       <p>personal training log / activity tracker</p>
       <a href="https://timleevt.github.io" target="_blank" rel="noreferrer">
